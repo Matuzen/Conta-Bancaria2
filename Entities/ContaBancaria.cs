6cc504d88c275a8eca18c237e9481a5751b17070
Matheus Alexandre <matheuscurses@gmail.com>
2022-11-07 17:29:05 -0300

    Conta bancária com opções a mais

---
+++ Entities/ContaBancaria.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using Conta.Entities;
+
+namespace Conta.Entities
+{
+     class ContaBancaria
+    {
+        public int Numero { get; private set; } // O n° da conta nao pode ser alterado, mas pode ser acessado
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = 0.0;
+        }
+
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular) // Reaproveitar os dois comandos 
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+            
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0; // Quantia + 5.0 que será descontado
+            
+        }
+
+        public void Boleto(double quantia)
+        {
+            Saldo -= quantia;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture); 
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Globalization;
+using Conta.Entities;
+
+
+namespace Conta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria;
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n) ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                contaBancaria = new ContaBancaria(numero, titular, depositoInicial);
+            }
+
+            else
+            {
+                contaBancaria = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.Write(contaBancaria);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Deposito(valor);
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(contaBancaria);
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Saque(valor);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(contaBancaria);
+           
+            Console.WriteLine();
+            Console.Write("Deseja agendar um pagamento? (s/n) ");
+            resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre com a data (dd/MM/yyyy): ");
+                DateTime data = DateTime.Parse(Console.ReadLine());
+                if (data < DateTime.Now)
+                {
+                    Console.WriteLine("Erro: A data nao pode ser menor do que hoje! ");
+                }
+
+                Console.Write("Digite o valor do boleto: ");
+                double boleto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (boleto > contaBancaria.Saldo)
+                {
+                    Console.WriteLine("Erro: O valor do boleto ultrapassa o seu saldo");
+                }
+
+                else
+                {
+                    contaBancaria.Boleto(boleto);
+                    Console.WriteLine("Dados atualizados: " + contaBancaria);
+                }
+            }
+
+        }
+    }
+}
